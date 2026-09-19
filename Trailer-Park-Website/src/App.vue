0a9e7a989c484b4c630347eae13ac1bcee523d4a
Kian Wheeler <79857505+commander1228@users.noreply.github.com>
2026-09-19 15:17:24 -0400

    init website commit

---
+++ Trailer-Park-Website/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <header>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </header>
+
+  <main>
+    <RouterView />
+  </main>
+</template>
