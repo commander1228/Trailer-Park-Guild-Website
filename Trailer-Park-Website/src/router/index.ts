153643e39ce69b751b2fcb8c9f499d58ec435f6b
Kian Wheeler <79857505+commander1228@users.noreply.github.com>
2026-09-19 21:03:52 -0400

    remove todo page

--- Trailer-Park-Website/src/router/index.ts
+++ Trailer-Park-Website/src/router/index.ts
@@ -1,12 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../pages/HomePage.vue'
-import TodoTestPage from '../pages/TodoTestPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'home', component: HomePage },
-    { path: '/todo-test', name: 'todo-test', component: TodoTestPage },
   ],
 })
 
